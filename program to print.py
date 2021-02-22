464c8b045a3d234d0fc36324a7030102b4111338
deep kumar <dk7759873560@gmail.com>
2021-02-22 10:36:01 +0530

    day6

---
+++ program to print.py
@@ -0,0 +1,28 @@
+# Write a program to print following :
+# **********
+# **********
+# **********
+# **********
+
+#
+# for i in range(5):
+#     for j in range(1,10):
+#         print("*",end=" ")
+#     print()
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(5):
+#     for j in range(i + 1):
+#         print("*",end=" ")
+#     print()
+
+
+
+
+
+
