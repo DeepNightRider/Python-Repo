3d995a5c9cdbd5a3722b09167582fcb12adbd0d3
deep kumar <dk7759873560@gmail.com>
2021-03-03 22:34:19 +0530

    Day14

---
+++ dictionary3.py
@@ -0,0 +1,11 @@
+# Below are the two lists convert it into the dictionary
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+dic={}
+dic['Ten']=10
+dic['Twenty']=20
+dic['Thirty']=30
+print(dic)
---
+++ dictionary5.py
@@ -0,0 +1,25 @@
+# Access the value of key ‘history’
+# sampleDict = {
+#     "class":{
+#         "student":{
+#             "name":"Mike",
+#             "marks":{
+#                 "physics":70,
+#                 "history":80
+#             }
+#         }
+#     }
+# }
+
+sampleDict = {
+    "class":{
+        "student":{
+            "name":"Mike",
+            "marks":{
+                "physics":70,
+                "history":80
+            }
+        }
+    }
+}
+print(sampleDict['class']['student']['marks']['history'])
